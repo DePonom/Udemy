b5c8ec85190957b68dcfaf7a86b8f77c29fd89a3
DePonom <fot-set@ya.ru>
2022-07-29 22:35:58 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+
+alert(`Привет ${user}`)
+
+document.write(`httt://${category}/5`);
+
+console.log(4 + +'-6');
+
+const isChecked = true,
+    isClose =false;
+
+console.log(isChecked && !isClose);
+
+
